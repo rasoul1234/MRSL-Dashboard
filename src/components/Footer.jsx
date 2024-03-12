5fa0beb9dfaee85d9266b00924bb58c8f13c9eb3
rasoul1234 <rasoul.sahibbzadah@auaf.edu.af>
2024-03-12 12:59:56 +0430

    Complete Admin Dashboard App

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24 text-center">
+    <p className="dark:text-gray-200 text-gray-700 m-20">
+      © 2024 All rights reserved by MRSL-DataView.com
+    </p>
+    <span style={{ color: 'green', fontSize: '14px' }}>Unlocking the Power of Data, MRSL leads the way towards insightful solutions and limitless possibilities.</span>
+  </div>
+);
+
+export default Footer;
